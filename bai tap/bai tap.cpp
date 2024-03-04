f4e3b80886e798aabb262f679a5ccd9d27723a4c
ADMIN <ADMIN@LAB1-MAY19>
2024-03-04 08:43:42 +0700

    Add project files.

---
+++ bai tap/bai tap.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello world!\n";
+    int i = 0, so;
+    cout << "nhap so:";
+    cin >> so;
+    while (i <= so)
+    {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
